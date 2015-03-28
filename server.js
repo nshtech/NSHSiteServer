34c4d25cf68574c3f3261674a2821efb28530f2a
gwcmk <gregwcmkim@gmail.com>
2015-03-28 00:54:05 -0400

    Added handlebars

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
+var fs = require('fs');
+var hbs = require('hbs');
+
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var path = require('path');
 
+hbs.registerPartials(__dirname + '/partials');
+
 server.listen(process.env.PORT || 3000, function(){
-  console.log('listening on port 8080');
+  console.log('listening on port 3000');
 });
 
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '');
 app.use(express.static(path.join(__dirname, '')));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index-2.html');
+  res.render('index-2');
 });
 app.get('/pf', function(req, res){
-  res.sendFile(__dirname + '/portfolio-2.html');
+  res.render('portfolio-2');
 });
 app.get('/apply', function(req, res){
-  res.sendFile(__dirname + '/apply.html');
+  res.render('apply');
 });
 app.get('/team', function(req, res){
-  res.sendFile(__dirname + '/team.html');
+  res.render('team');
 });
 app.get('/about', function(req, res){
-  res.sendFile(__dirname + '/about-us.html');
+  res.render('about-us');
 });
 
 
