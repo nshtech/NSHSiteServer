d29ecdffe6508749c0ab5961283802c07a17e8c3
Matt Hong <mhong19414@gmail.com>
2015-05-08 01:28:47 -0500

    Fixes smooth scrolling action

--- _layout/js/splash.js
+++ _layout/js/splash.js
@@ -1,12 +1,12 @@
 $(function(){
-$('a[href^="#"]').on('click',function (e) {
+$('.smoothScroll').on('click',function (e) {
         e.preventDefault();
 
         var target = this.hash;
         var $target = $(target);
 
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top - 91
+            'scrollTop': $target.offset().top - 90
         }, 800, 'swing');
     });
 });
